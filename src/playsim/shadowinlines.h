2c3ea95a92643576d784a4f77e5884cf9a1b4567
Kartinea <scott.stanfield81@gmail.com>
2024-05-06 16:02:05 -0700

    Fix invisibility sphere effect on enemies

--- src/playsim/shadowinlines.h
+++ src/playsim/shadowinlines.h
@@ -85,12 +85,12 @@ inline bool AffectedByShadows(AActor* self)
 
 inline AActor* CheckForShadows(AActor* self, AActor* other, DVector3 pos, double& penaltyFactor)
 {
-	// KARTINEA: This broke invis completely since if nothing was in between the 2 actors, it would return nullptr which is treated as no aiming alteration
-	// If the other actor is shadowed then just return it
-	// This is probably not quite right, but I don't care
-	if (other && (other->flags & MF_SHADOW))
-		return other;
-    return (self->flags9 & MF9_DOSHADOWBLOCK) ? P_CheckForShadowBlock(self, other, pos, penaltyFactor) : nullptr;
+	if ((other && (other->flags & MF_SHADOW)) || (self->flags9 & MF9_DOSHADOWBLOCK))
+	{
+		AActor* shadowBlock = P_CheckForShadowBlock(self, other, pos, penaltyFactor);
+		return shadowBlock ? shadowBlock : other;
+	}
+	return nullptr;
 }
 
 inline AActor* PerformShadowChecks(AActor* self, AActor* other, DVector3 pos, double& penaltyFactor)
